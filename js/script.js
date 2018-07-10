9fddbb74048b71560a822384bf38ce574df35871
Terry <tshenker@gmail.com>
2018-07-10 05:42:09 +0000

    added styles, error checking and playback of videos

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
-function test(){
+$('#button-getVideos').click(function(e) {
+    e.preventDefault();
+    let searchString = $('#input-searchTerms').val();
+    if(!searchString) {
+        $('fieldset').append('<span class="warning-message"><i class="fas fa-exclamation-triangle"></i> Input a search term</span>');
+    }
+    else {
+        findYouTubeVideos(searchString);    
+    }
+});
+
+
+function findYouTubeVideos(searchString) {
 
     const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
     const API_KEY = "AIzaSyDEwfk3b9G0Oap6N7GHBeDHPzhnXxxr5hM";
-    let searchTerm = "surfboards";
-    
-const settings = {
+    let searchTerm = searchString;
+
+    const settings = {
         url: YOUTUBE_SEARCH_URL,
         data: {
             part: 'snippet',
@@ -14,10 +26,9 @@ const settings = {
         },
         dataType: 'json',
         type: 'GET',
-        success: function () {
-            // console.log(search_results);
+        success: function() {
             displayResults(search_results);
-	    },
+        },
         error: function() {
             console.log("An error occurred");
         }
@@ -26,20 +37,20 @@ const settings = {
     let search_results = $.ajax(settings);
 }
 
-function displayResults (search_results) {
+function displayResults(search_results) {
     console.log(search_results);
     let result_arr = search_results.responseJSON.items;
-    result_arr.map( result => {
-        $('.search-results').append(`
+    let resultString = "";
+    result_arr.map(result => {
+        resultString += `
             <h2>${result.snippet.title}</h2>
             <p>${result.snippet.description}</p>
             <p>Channel: ${result.snippet.channelTitle}</p>
             <p>Published: ${result.snippet.publishedAt}</p>
-            <img src=${result.snippet.thumbnails.medium.url} alt="Image of ${result.snippet.title}" />
-        `)
+            <iframe src="https://www.youtube.com/embed/${result.id.videoId}" allow="autoplay; encrypted-media" width="350" height="200" frameborder="0" allowFullScreen></iframe>`
     });
+    $('.search-results').html(resultString);
 }
 
-$(test)
-
-
+// start page with 'safe default search term to populate it'
+$(findYouTubeVideos('surfboards'))
